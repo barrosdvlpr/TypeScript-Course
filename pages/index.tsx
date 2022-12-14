81f4fad04d3292236adbbb0e3e7045c09e18d88b
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-14 09:44:10 -0300

    Create First Generic in TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 
 export default function Home() {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
+  return ("Hello World")
 }
+
+export function fill<T>(array:any[], value:T):T[]{
+  return array.map(() => value)
+}
+
+const result = fill([1,2,3], "a")
+console.log(result)
+
+const values = fill(["a","b","c"], 4)
+values.map(x => x / 2)
+console.log(values)
