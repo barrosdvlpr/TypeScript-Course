f8cc8b3d14a76f0e00142ee30a4d25f36b394201
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-14 09:02:15 -0300

    explore typescript writting functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 
-export default function Home() {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
-}
+type Salutation = {greeting:string, name:string}
+
+export default function greet({greeting, name}:Salutation):string {
+  return `${greeting}, ${name}!`
+} 
+
+const message:string = greet({greeting:"Hello", name:"John"})
+
+console.log(message)
