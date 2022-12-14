103586f2fb90992ce7ce43ef287d2dc2a5507d21
barrosdvlpr <barrosdvlpr@gmail.com>
2022-12-14 10:02:06 -0300

    use type definitions-on npm with @types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import _ from "lodash";
+import $ from "jquery";
+
+console.log(_.uniq([1,1,2]))
+
+$().add
+_.camelCase()
 
 export default function Home() {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
+  return ("Hello World")
 }
